3674d8b1de92b78d6f2d5226c87711b1baf75f87
DESKTOP-8DV2N22\h250413 <mkhr105052@gmail.com>
2025-07-10 09:04:46 +0900

    エネミーC修正 壁やプレイヤーに刺さるのでコライダー追加 攻撃パターン調整 接触ダメージ廃止

--- Assets/FormC/Scripts/EnemyCController.cs
+++ Assets/FormC/Scripts/EnemyCController.cs
@@ -3,16 +3,17 @@ using Unity.VisualScripting;
 using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.PlayerSettings;
 
 // 鶏エネミー
 public class EnemyCCntroller : MonoBehaviour
 {
     public float moveRadius = 5f;   // うろつく範囲
-    public float sensingRange = 8f; // プレイヤー検知範囲
+    public float sensingRange = 6f; // プレイヤー検知範囲
 
-    public GameObject damageObj; // 投下するやつ
-    public int dropCount = 5;    // 投下数
-    public float dropRange = 5f; // 投下範囲
+    public GameObject dropObj;      // 投下物
+    public int dropCount = 4;       // 投下数
+    public float scatterPower = 3f; // 投下範囲
 
     public float attackJumpHeight = 7f; // 攻撃時のジャンプの高さ
 
@@ -41,16 +42,19 @@ public class EnemyCCntroller : MonoBehaviour
             return;
         }
 
-        if (damageObj == null)
+        if (dropObj == null)
         {
-            Debug.LogError("damageObjが見つかりません！");
+            Debug.LogError("dropObjが見つかりません！");
             enabled = false;
             return;
         }
 
         // 子オブジェクトのコライダーを取得
         damageArea = transform.Find("DamageArea").GetComponent<SphereCollider>();
-        damageArea.enabled = false;
+        if (damageArea != null)
+        {
+            damageArea.enabled = false;
+        }
 
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
@@ -104,7 +108,7 @@ public class EnemyCCntroller : MonoBehaviour
             if (watchTime > attackWaitTime)
             {
                 // 攻撃
-                StartCoroutine("Attack", player);
+                StartCoroutine("Attack");
 
                 // 次回の攻撃待機時間をセット
                 SetAttackWaitTime();
@@ -166,6 +170,8 @@ public class EnemyCCntroller : MonoBehaviour
 
         // 座標を相対化
         Vector3 pos = targetPos - transform.position;
+
+        // 旋回
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(pos), Time.deltaTime * 5f);
 
         // 時間を加算
@@ -177,7 +183,7 @@ public class EnemyCCntroller : MonoBehaviour
     }
 
     // 攻撃
-    IEnumerator Attack(Transform target)
+    IEnumerator Attack()
     {
         //Debug.Log("Attack");
         isAttack = true;
@@ -189,53 +195,94 @@ public class EnemyCCntroller : MonoBehaviour
         animator.SetBool("jump", true);
 
         // 接触ダメージ有効化
-        damageArea.enabled = true;
+        if (damageArea != null)
+        {
+            damageArea.enabled = true;
+        }
 
-        Vector3 pos = target.position - transform.position;
+        int dropped = 0;
+        float timer = 0f;
         float velocityY = attackJumpHeight;
         float lastY = 0f;
-        bool isDone = false;
+        float initZ = 0f;
+        Vector3 diffPos = Vector3.zero;
 
         // 空中にいる間繰り返す
         while (!controller.isGrounded || velocityY > 0f)
         {
-            // 前フレーム時点の高さを保存
-            lastY = transform.position.y;
-
             // 重力をかける
             velocityY += Physics.gravity.y * Time.deltaTime;
 
-            // 旋回
-            transform.rotation = Quaternion.LookRotation(pos);
-
-            // 移動
-            Vector3 move = Vector3.up * velocityY + transform.forward * 7f;
-            controller.Move(move * Time.deltaTime);
-
-            // 最高点なら何かを投下
-            if (!isDone && lastY > transform.position.y)
+            // ゆっくり落とす
+            if (velocityY < -1.5f)
             {
-                for (int i = 0; i < dropCount; i++)
+                velocityY = -1.5f;
+            }
+
+            // 上昇中
+            if (lastY < transform.position.y)
+            {
+                // プレイヤー位置取得
+                Transform player = PlayersManager.Instance.GetPlayer();
+
+                // 上昇中は旋回する
+                diffPos = player.position - transform.position;
+                transform.rotation = Quaternion.LookRotation(new Vector3(diffPos.x, 0, diffPos.z));
+
+                // 初回だけ移動距離をセット
+                initZ = initZ > 0 ? initZ : diffPos.z;
+            }
+            // 下降中
+            else
+            {
+                // 投下数だけ投下する
+                if (dropped < dropCount && timer <= 0f)
                 {
+                    // 投下物生成
                     GameObject obj = Instantiate(
-                        damageObj,
-                        new Vector3(transform.position.x, transform.position.y - 0.2f, transform.position.z - (i * 0.75f)),
-                        Quaternion.Euler(0, 90, 90)
+                        dropObj,
+                        new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z - 1f),
+                        Quaternion.Euler(0, 90, 135)
                     );
+                    // 投下
                     obj.GetComponent<Rigidbody>().AddForce(
-                        new Vector3(Random.Range(-dropRange, dropRange), -3f, Random.Range(-dropRange, dropRange)),
+                        new Vector3(Random.Range(-scatterPower, scatterPower), -2f, Random.Range(-scatterPower, scatterPower)),
                         ForceMode.Impulse
                     );
+                    // 投下数追加
+                    dropped++;
+
+                    // タイマー再セット
+                    timer = 0.25f;
                 }
-                // 投下済みフラグオン
-                isDone = true;
+                else
+                {
+                    // タイマー更新
+                    timer -= Time.deltaTime;
+                }
             }
+
+            // 移動量を作る(下降中はまっすぐ移動)
+            Vector3 move = new Vector3(
+                diffPos.x,
+                velocityY,
+                initZ * 2f
+            );
+
+            // このフレームの高さを保存
+            lastY = transform.position.y;
+
+            // 移動
+            controller.Move(move * Time.deltaTime);
 
             yield return null;
         }
 
         // 接触ダメージ無効化
-        damageArea.enabled = false;
+        if (damageArea != null)
+        {
+            damageArea.enabled = false;
+        }
 
         // アニメーションを戻す
         animator.SetBool("jump", false);
@@ -243,7 +290,6 @@ public class EnemyCCntroller : MonoBehaviour
         // NavMeshAgentを戻す
         agent.enabled = true;
 
-        // 攻撃状態を初期化
         watchTime = 0;
         isAttack = false;
     }
@@ -251,6 +297,6 @@ public class EnemyCCntroller : MonoBehaviour
     // 攻撃待機時間をセット
     void SetAttackWaitTime()
     {
-        attackWaitTime = Random.Range(0f, 1.5f);
+        attackWaitTime = Random.Range(0.2f, 1.2f);
     }
 }
